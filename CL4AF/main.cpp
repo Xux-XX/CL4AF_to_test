7d0cb21b27b4a32ae7c83cc4c197f2c5f2a9982a
Xux <2166902048@qq.com>
2024-10-21 19:03:01 +0800

    回到只测试tgf的版本

--- CL4AF/main.cpp
+++ CL4AF/main.cpp
@@ -4,10 +4,6 @@ int main(int argc, char**argv) {
     Solver solver;
     solver.read_file(argv[1]);
     solver.solve();
-    
-    std::string s(argv[1]);
-    if (s.ends_with(".af")){
-        std::cout<<(solver.self_check() ? "true" : "false")<<std::endl;
-    }
+
     return 0;
 }
